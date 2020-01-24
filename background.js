76f583486cacff17f0e70df0223cfa7a0734c5c4
isdapro <isdapro@users.noreply.github.com>
2020-01-24 19:34:29 +0530

    Updated to Asynchronously Auto Click

--- background.js
+++ background.js
@@ -7,6 +7,94 @@ function dataURLtoBlob(dataurl) {
     return new Blob([u8arr], {type:mime});
 }
 
+async function sendMessagePromise(tabId,item)
+{
+  console.log(item);
+  return new Promise((resolve,reject) => {
+    chrome.tabs.sendMessage(tabId, item, response => {
+      if (response.complete){
+        resolve();
+      }
+      else {
+        reject('Wrong');
+      }
+    });
+
+  });
+}
+
+
+async function clickUnfollow(dataURL)
+{
+  let image = dataURL;
+  let blobimage = dataURLtoBlob(image);
+  let formdata = new FormData();
+  formdata.append('file',blobimage,'InitialCapture.png');
+  let xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.open('POST','SERVER_NAME');
+  xhr.withCredentials = true;
+  xhr.onload = async function () {
+    if (this.status===200)
+    {
+      let data = this.response;
+      console.log(data);
+      let x = data.x;
+      let y = data.y;
+      let n = x.length;
+
+      for (let i=0;i<n;i++)
+      {
+        console.log(x[i]);
+        setTimeout(async function() {
+          chrome.tabs.query({active:true, currentWindow:true}, async function(tabs){
+          var activeTab = tabs[0];
+          await sendMessagePromise(activeTab.id,{"message":"base_click","x":x[i],"y":y[i]});
+
+
+        });
+      }, 500);
+        
+        
+      }
+    }
+  };
+  xhr.send(formdata);
+}
+
+function clickConfirmation()
+{
+  return new Promise(resolve => {
+    chrome.tabs.onUpdated.addListener(function(tabid,info) {
+      if (info.status === 'complete'){
+        chrome.tabs.captureVisibleTab({"format":"png"},function(dataURL) {
+          let image = dataURL;
+          let blobimage = dataURLtoBlob(image);
+          let formdata = new FormData();
+          formdata.append('file',blobimage,'ConfirmationImage.png');
+          let xhr = new XMLHttpRequest();
+          xhr.responseType = 'json';
+          xhr.open('POST','SERVER_NAME');
+          xhr.withCredentials = true;
+          xhr.onload = function () {
+          if (this.status === 200)
+            {
+              let evconfirm = document.createEvent("MouseEvents");
+              evconfirm.initEvent("click",true,true);
+              document.elementFromPoint(this.response["x"],this.response["y"]).dispatchEvent(evconfirm);
+            }
+          };
+  
+  
+          xhr.send(formdata);
+          chrome.tabs.onUpdated.removeListener(onUpdated);
+          resolve();
+        });
+      }
+    });
+  })
+
+}
 
 
 chrome.browserAction.onClicked.addListener(function(tab){
@@ -23,31 +111,10 @@ chrome.runtime.onMessage.addListener(
     function(request,sender,sendResponse){
         if (request.message==="take_screenshot"){
             chrome.tabs.captureVisibleTab({"format":"png"},function(dataURL){
-                let image = dataURL;
-                let blobimage = dataURLtoBlob(image);
-                console.log(image);
-                let formdata = new FormData();
-                formdata.append('file',blobimage,'ExtensionImage.png');
-                let xhr = new XMLHttpRequest();
-                xhr.responseType = 'blob';
-                xhr.open('POST','SERVER_NAME');
-                xhr.withCredentials = true;
-                xhr.onload = function () {
-                    if (this.status === 200)
-                    {
-                        let urlf = URL.createObjectURL(this.response);
-                        chrome.downloads.download({
-                            
-                            url:urlf,
-                            filename: 'just_checking.png'
-                        });
-                    }
-                    else
-                        console.error(xhr);
-                };
-                xhr.send(formdata);
+              clickUnfollow(dataURL);
+
             });
-            
+
 
         }
     }
--- content.js
+++ content.js
@@ -3,8 +3,31 @@ chrome.runtime.onMessage.addListener(
     function(request,sender,sendResponse){
         if (request.message==="clicked_browser_action"){
             window.scroll(0,0);
+            function printMousePos(event) {
+                console.log(event.clientX);
+                console.log(event.clientY);
+              }
+              
+            document.addEventListener("click", printMousePos);
             chrome.runtime.sendMessage({"message":"take_screenshot"});
 
+        }
+        else if (request.message==="base_click"){
+            let ev = document.createEvent("MouseEvents");
+            let ev2 = document.createEvent("MouseEvents");
+            let x = request.x;
+            let y = request.y;
+            ev.initMouseEvent("click",true,true,window,0,0,0,x,y,false,false,false,false,0,null);
+            ev2.initMouseEvent("click",true,true,window,0,0,0,748,547,false,false,false,false,0,null);
+            document.elementFromPoint(x,y).dispatchEvent(ev);
+            document.activeElement.dispatchEvent(ev2)
+            console.log("CLICKED");
+            //setTimeout(function(){
+               // document.activeElement.dispatchEvent(ev2)},
+               // 500);
+            sendResponse({complete:true});
+            return true;
+           // 
         }
     }
 );
